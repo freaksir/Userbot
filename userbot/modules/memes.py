e9d2bc3efc98c3b68eaa248c19ae269c1fadf072
freaksir <54507778+freaksir@users.noreply.github.com>
2019-09-01 16:49:06 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -931,7 +931,7 @@ async def hacking (hacked):
         if await hacked.get_reply_message():
             await hacked.edit(
                 "`Targeted Account Hacked successfully 😎......`\n"
-                "`Pay Rs.69,000 to` [Spyder](https://t.me/spyderzzz/) `to Remove This Hack...`\n"
+                "`Pay Rs.69,000 to` [Freak](https://t.me/freaksir/) `to Remove This Hack...`\n"
             )
 			  
 @register(outgoing=True, pattern="^.kill$")
